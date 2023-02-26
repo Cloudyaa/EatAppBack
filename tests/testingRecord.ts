3020ba4b39409bc7ff5779e816ef9954bc8c9aff
Klaudia Olczyk <klaudiaaolczyk@gmail.com>
2023-02-26 16:22:43 +0000

    Created testing object for tests

---
+++ tests/testingRecord.ts
@@ -0,0 +1,12 @@
+import { NewProductEntity } from '../types';
+
+export const testProduct: NewProductEntity = {
+  name: 'Test name',
+  price: 99.99,
+  description: 'Test description',
+  energy: 100,
+  fat: 20.5,
+  saturates: 20.5,
+  sugars: 20.5,
+  salt: 20.5,
+};
