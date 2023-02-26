bd1e0fabbd6c8238f9ff2a2310fce2dada95ae92
Klaudia Olczyk <klaudiaaolczyk@gmail.com>
2023-02-26 20:03:07 +0000

    Added all endpoints

--- routes/products.router.ts
+++ routes/products.router.ts
@@ -1,7 +1,40 @@
 import { Router } from 'express';
+import { ProductRecord } from '../records/product.record';
 
 export const productsRouter = Router();
 
-productsRouter.get('/', async (req, res) => {
-  res.json('test');
-});
+productsRouter
+  // get all products when empty string provided
+  .get('/search/:name?', async (req, res) => {
+    const found = await ProductRecord.find(req.params.name ?? '');
+
+    const products = found.map((product) => {
+      return {
+        ...product,
+        qtyInBasket: 0, // req.body.qtyInBasket??
+      };
+    });
+
+    if (products.length === 0) {
+      res.json(`No results were found matching ${req.params.name}`);
+    } else {
+      res.json(products);
+    }
+  })
+
+  // get one product if id is found
+  .get('/:id', async (req, res) => {
+    const product = await ProductRecord.getOne(req.params.id);
+    if (product === null) {
+      res.json(`Cannot find product with id ${req.params.id}`);
+    } else {
+      res.json(product);
+    }
+  })
+
+  // send data from FE to db
+  .post('/', async (req, res) => {
+    const product = new ProductRecord(req.body);
+    await product.insert();
+    res.json(product);
+  });
