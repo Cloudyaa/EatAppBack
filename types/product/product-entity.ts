4bd8269472a08525f03c316087537c0496d23846
Klaudia Olczyk <klaudiaaolczyk@gmail.com>
2023-02-26 15:18:47 +0000

    Added types for ProductRecord

--- types/product/product-entity.ts
+++ types/product/product-entity.ts
@@ -1,6 +1,22 @@
-export type ProductEntity = {
+export interface SimpleProductEntity {
   id: string;
   name: string;
   price: number;
   qtyInBasket: number;
-};
+}
+
+export interface Nutrition {
+  energy: number;
+  fat: number;
+  saturates: number;
+  sugars: number;
+  salt: number;
+}
+
+export interface ProductEntity extends SimpleProductEntity, Nutrition {
+  description: string;
+}
+
+export interface NewProductEntity extends Omit<ProductEntity, 'id'> {
+  id?: string;
+}
