df54cfae2b7035d661c0b759e7905ce7dd7be042
Klaudia Olczyk <klaudiaaolczyk@gmail.com>
2023-02-26 16:10:36 +0000

    Added RateLimiter and productsRouter

--- index.ts
+++ index.ts
@@ -1,20 +1,29 @@
-import cors from 'cors';
 import express, { json } from 'express';
+import cors from 'cors';
+import rateLimit from 'express-rate-limit';
 import 'express-async-errors';
 import 'express-rate-limit';
 import { handleError } from './utlis/errors';
+import { config } from './config/config';
+import { productsRouter } from './routes/products.router';
 
 const app = express();
 
 app.use(
   cors({
-    origin: 'http://localhost:3000',
+    origin: config.corsOrigin,
   }),
 );
 app.use(json());
+app.use(
+  rateLimit({
+    windowMs: 5 * 60 * 1000, // 5 minutes
+    max: 100, // Limit each IP to 100 requests per 'window' (here, per 5 minutes)
+  }),
+);
 
 //routes
-
+app.use('/products', productsRouter);
 //end of routes
 app.use(handleError);
 
