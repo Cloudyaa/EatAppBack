d38a9e83c4dd57aeba50ffb91fe95cf4a0fbf7d5
Klaudia Olczyk <klaudiaaolczyk@gmail.com>
2023-03-13 18:45:35 +0000

    Simplified regexp

--- user/validateEmail.ts
+++ user/validateEmail.ts
@@ -2,7 +2,7 @@ import { ValidationError } from '../utlis/errors';
 
 export const validateEmail = (email: string) => {
   const emailRegex =
-    /^(?!.*@{2})(?!.*[-_.]{3})[^\s!?\-;:'"*#&]+(?:[\w.-]*[\w])?@(?!-)(?:[a-zA-Z0-9-]{0,63}(?<!-)\.)+(?!-)[a-zA-Z]{2,}$/;
+    /^(?!.*@{2})(?!.*[-_.]{3})[^\s!?\-;:'"*#&]+(?:[\w.-]*\w)?@(?!-)(?:[a-zA-Z0-9-]{0,63}(?<!-)\.)+(?!-)[a-zA-Z]{2,}$/;
 
   if (!email) {
     throw new ValidationError('Email must be provided');
