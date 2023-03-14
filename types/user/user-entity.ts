511375ae8d7c93a279b4eeef06aed53347e9a7ce
Klaudia Olczyk <klaudiaaolczyk@gmail.com>
2023-03-14 21:53:06 +0000

    Added SafeUserType and AuthUser interface

--- types/user/user-entity.ts
+++ types/user/user-entity.ts
@@ -1,18 +1,26 @@
 export interface SimpleUserEntity {
   userId: string;
   email: string;
-  password: string;
+  role: string;
 }
 
-export interface UserEntity extends SimpleUserEntity {
+export interface SafeUserEntity extends SimpleUserEntity {
   createdAt: string;
   updatedAt: string;
 }
 
-export interface SecureUserEntity extends Omit<UserEntity, 'password'> {
-  password?: string;
+export interface UserEntity extends SimpleUserEntity, SafeUserEntity {
+  password: string;
 }
 
-export interface NewUserEntity extends Omit<UserEntity, 'userId'> {
+export interface NewUserEntity extends Omit<UserEntity, 'userId' | 'createdAt' | 'updatedAt'> {
   userId?: string;
+  createdAt?: string;
+  updatedAt?: string;
+}
+
+export interface AuthUser {
+  userId: string;
+  role: string;
+  exp: number;
 }
