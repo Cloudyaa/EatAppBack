cf4ec8afec208ecda64befb041e022eb4c57ddfa
Klaudia Olczyk <klaudiaaolczyk@gmail.com>
2023-03-14 21:51:47 +0000

    Added user authentication for access to user dashboard

--- middleware/auth.middleware.ts
+++ middleware/auth.middleware.ts
@@ -1,7 +1,7 @@
 import { NextFunction, Request, Response } from 'express';
 import jwt, { Secret } from 'jsonwebtoken';
 import { AuthUser } from '../types';
-import { UserRecord } from '../records/user.record';
+import { UserRecord } from '../account/user/user.record';
 
 const JWT_SECRET: Secret = process.env.JWT_SECRET || 'default_secret';
 
@@ -17,21 +17,33 @@ export const authMiddleware =
 
     try {
       const decodedToken = jwt.verify(token, JWT_SECRET) as AuthUser;
+
+      // if user is not authorized to access the requested dashboard
+      if (requiredRole === 'user' && decodedToken.userId !== req.params.id) {
+        return res.status(401).json({ message: 'You are not authorized to access this dashboard' });
+      }
+
+      // if role of the user in the decoded token is different from the requiredRole
       if (decodedToken.role !== requiredRole) {
         return res.status(403).json({ message: 'Forbidden' });
       }
 
-      const userRole = await UserRecord.getRole(decodedToken.userId);
+      const user = await UserRecord.findById(decodedToken.userId);
 
       // if user wants access to admin panel
-      if (userRole !== requiredRole) {
+      if (user && user.role !== requiredRole) {
         return res.status(403).json({ message: 'Forbidden' });
       }
+
       next();
     } catch (error) {
       console.error(error);
 
-      // if not logged person wants access to admin panel
-      res.status(401).json({ message: 'Unauthorized' });
+      // if not logged person wants access to admin panel or token expired
+      if (error instanceof jwt.TokenExpiredError) {
+        res.status(401).json({ message: 'Token has expired' });
+      } else {
+        res.status(401).json({ message: 'Unauthorized' });
+      }
     }
   };
