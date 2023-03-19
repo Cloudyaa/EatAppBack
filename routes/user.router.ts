5778a2eb02291925e57127f904c4bfdc6b62d752
Klaudia Olczyk <klaudiaaolczyk@gmail.com>
2023-03-19 17:34:03 +0000

    Fixed json response

--- routes/user.router.ts
+++ routes/user.router.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 import { authMiddleware } from '../middleware/auth.middleware';
-import { UserRecord } from "../records";
+import { UserRecord } from '../records';
 
 export const userRouter = Router();
 
@@ -8,11 +8,11 @@ userRouter.get('/dashboard/:id', authMiddleware('user'), async (req, res) => {
   const user = await UserRecord.findById(req.params.id);
 
   if (!user) {
-    return res.status(404).json({
+    res.status(404).json({
+      status: res.statusCode,
       message: 'User not found',
     });
   }
-  res.json({
-    user,
-  });
+
+  res.json(user);
 });
